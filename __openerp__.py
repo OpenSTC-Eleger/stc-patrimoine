01f415154734c19b9f8b7012507d5b8fbd16e45b
plancher bruno <bruno.plancher@gmail.com>
2013-05-23 09:08:38 +0200

    changed prefix 'openstc_prets' to openstc, improved fr_FR translation

--- __openerp__.py
+++ __openerp__.py
@@ -8,7 +8,7 @@
 {
     "name": "openstc_patrimoine",
     "version": "0.1",
-    "depends": ["openstc_prets",],
+    "depends": ["openstc",],
     "author": "BP",
     "category": "Category",
     "description": """
